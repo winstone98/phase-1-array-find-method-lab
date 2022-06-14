572fdfd8df574b71348579d520701bc9531ed342
root <root@HomeLPT-001.localdomain>
2022-06-14 10:41:45 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+//https://github.com/winstone98/phase-1-array-find-method-lab.
+function superbowlWin(record) {
+    let winningYearObject = record.find(element => element.result === "W")
+    if (typeof (winningYearObject) === "object") {
+        return winningYearObject.year
+    }
+}
